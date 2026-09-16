b0ae736ea768793152d5e9c5d2de23faa96d9eaa
Samuel He <37854805+shungjiHe@users.noreply.github.com>
2026-09-16 13:06:35 +0800

    Add initial sentence bank with common phrases

---
+++ sentence_bank.js
@@ -0,0 +1,14 @@
+const sentenceBank = [
+    "Are you free at three this afternoon?",
+    "Does three work for you?",
+    "Does that work for you?",
+    "That works for me.",
+    "That works.",
+    "I'll keep that in mind.",
+    "I'll get back to you later.",
+    "How long will it take?",
+    "Do you have time for a quick chat?",
+    "Sure, what's on your mind?",
+    "I didn't catch that.",
+    "I'm not following."
+];
